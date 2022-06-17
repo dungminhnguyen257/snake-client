4826c9aaa8ae18658ee75d4ba4b66ed14587cf4f
dungminhnguyen257 <bryan225577@gmail.com>
2022-06-17 19:39:38 +0000

    client set up and connect to the server

---
+++ play.js
@@ -0,0 +1,15 @@
+const { prototype } = require("events");
+const net = require("net");
+//establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  //interpret incoming data as text
+  conn.setEncoding("utf-8");
+
+  return conn;
+};
+console.log("Connecting...");
+connect();
