e4d03a51cb3d3b98c016aea097fca974ca588375
dungminhnguyen257 <bryan225577@gmail.com>
2022-06-20 17:54:57 +0000

    create chat bubbles, store const variables in constants.js

--- input.js
+++ input.js
@@ -1,28 +1,44 @@
+const {
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+} = require("./constants");
+
 let connection;
+
 const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
   const handleUserInput = (key) => {
     switch (key) {
       case "\u0003":
         process.exit();
-      case "w":
+      case MOVE_UP_KEY:
         connection.write("Move: up");
         break;
-      case "s":
+      case MOVE_DOWN_KEY:
         connection.write("Move: down");
         break;
-      case "a":
+      case MOVE_LEFT_KEY:
         connection.write("Move: left");
         break;
-      case "d":
+      case MOVE_RIGHT_KEY:
         connection.write("Move: right");
+        break;
+      case "q":
+        connection.write("Say: GG");
+        break;
+      case "e":
+        connection.write("Say: Nice");
         break;
     }
   };
+
   stdin.on("data", handleUserInput);
   return stdin;
 };
